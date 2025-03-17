b6124978b541cc4df6616634be539f7e1f32ebad
Muhammed Imdaad <muhammedimdaad16@gmai.com>
2025-03-18 00:30:51 +0530

    initial tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
 #include <gtest/gtest.h>
 
-class Soundex {};
+class Soundex
+{
+private:
+    auto zeroPad(const std::string&input) const
+    {
+        return input + "000";
+    }
+public:
+    auto encode(const std::string &input) const
+    {
+        return zeroPad(input);
+    }
+};
 
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord)
 {
     Soundex soundex;
+    auto encoded = soundex.encode("A");
+    ASSERT_EQ(encoded, "A000");
+}
+
+TEST(SoundexEncoding, PadsWithZerosToEnsureThreeDigits)
+{
+    Soundex soundex;
+    auto encoded = soundex.encode("|");
+    ASSERT_STREQ(encoded.c_str(), "|000");
 }
